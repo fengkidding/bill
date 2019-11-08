3b7ae1971bb38cdb189b8d255a78bc0449ca0f43
fengkidding <56989531+fengkidding@users.noreply.github.com>
2019-11-08 18:07:38 +0800

    Feign调用增加鉴权信息

---
+++ src/main/java/com/bill/interceptor/FeignRequestHeaderInterceptor.java
@@ -0,0 +1,29 @@
+package com.bill.interceptor;
+
+import com.bill.common.util.AuthContextUtils;
+import com.bill.model.constant.AuthConstant;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Feign调用增加鉴权信息
+ *
+ * @author f
+ * @date 2019-11-08
+ */
+public class FeignRequestHeaderInterceptor implements RequestInterceptor {
+
+    /**
+     * 请求头增加参数
+     *
+     * @param requestTemplate
+     */
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        String memberId = AuthContextUtils.getMemberId();
+        if (StringUtils.isNotBlank(memberId)) {
+            requestTemplate.header(AuthConstant.MEMBER_ID, memberId);
+        }
+    }
+}
---
+++ src/main/java/com/bill/model/constant/AuthConstant.java
@@ -0,0 +1,38 @@
+package com.bill.model.constant;
+
+/**
+ * 鉴权验证常量
+ *
+ * @author f
+ * @date 2019-11-07
+ */
+public class AuthConstant {
+
+    /**
+     * id
+     */
+    public static final String MEMBER_ID = "memberId";
+
+    /**
+     * 用户名
+     */
+    public static final String USER_NAME = "userName";
+    
+    /**
+     * token名称
+     */
+    public static final String COOKIE_NAME = "token";
+
+    /**
+     * 一周
+     */
+    public static final long LONG_SESSION = 604800000;
+
+    /**
+     * 一小时
+     */
+    public static final long SHORT_SESSION = 3600000;
+
+    private AuthConstant() {
+    }
+}
