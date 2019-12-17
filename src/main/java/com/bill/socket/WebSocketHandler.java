02a2c78aa44630e48585b2a402e06c9682afd3df
fengkidding <56989531+fengkidding@users.noreply.github.com>
2019-12-17 16:53:43 +0800

    Netty

---
+++ src/main/java/com/bill/socket/WebSocketHandler.java
@@ -0,0 +1,162 @@
+package com.bill.socket;
+
+import com.bill.common.log.LogBackUtils;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+
+import java.util.Date;
+
+/**
+ * netty业务处理
+ *
+ * @author f
+ * @date 2019-12-17s
+ */
+public class WebSocketHandler extends SimpleChannelInboundHandler<Object> {
+
+    private WebSocketServerHandshaker handshaker;
+
+    /**
+     * 建立连接时调用
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        NettyConfig.addGroup(ctx.channel());
+    }
+
+    /**
+     * 关闭连接时调用
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        NettyConfig.removeGroup(ctx.channel());
+    }
+
+    /**
+     * 读完成
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    /**
+     * 异常处理
+     *
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable e) throws Exception {
+        e.printStackTrace();
+        LogBackUtils.error("netty异常：", e);
+        ctx.close();
+    }
+
+    /**
+     * 接收消息
+     *
+     * @param context
+     * @param msg
+     * @throws Exception
+     */
+    protected void messageReceived(ChannelHandlerContext context, Object msg) throws Exception {
+        if (msg instanceof FullHttpRequest) {
+            this.handHttpRequest(context, (FullHttpRequest) msg);
+        } else if (msg instanceof WebSocketFrame) {
+            this.handWebsocketFrame(context, (WebSocketFrame) msg);
+        }
+
+    }
+
+    /**
+     * 业务处理
+     *
+     * @param ctx
+     * @param frame
+     */
+    private void handWebsocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        if (frame instanceof CloseWebSocketFrame) {
+            this.handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+        }
+
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+        } else if (!(frame instanceof TextWebSocketFrame)) {
+            System.out.println("目前我们不支持二进制消息");
+            throw new RuntimeException("【" + this.getClass().getName() + "】不支持消息");
+        } else {
+            String request = ((TextWebSocketFrame) frame).text();
+            System.out.println("服务端收到客户端的消息====>>>" + request);
+            TextWebSocketFrame tws = new TextWebSocketFrame((new Date()).toString() + ctx.channel().id() + " ===>>> " + request);
+            NettyConfig.getGroup().writeAndFlush(tws);
+        }
+    }
+
+    /**
+     * @param ctx
+     * @param req
+     */
+    private void handHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
+        if (req.getDecoderResult().isSuccess() && "websocket".equals(req.headers().get("Upgrade"))) {
+            WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory("ws://localhost:10050/websocket", null, false);
+            this.handshaker = wsFactory.newHandshaker(req);
+            if (this.handshaker == null) {
+                WebSocketServerHandshakerFactory.sendUnsupportedWebSocketVersionResponse(ctx.channel());
+            } else {
+                this.handshaker.handshake(ctx.channel(), req);
+            }
+
+        } else {
+            this.sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+        }
+    }
+
+    /**
+     * @param ctx
+     * @param req
+     * @param res
+     */
+    private void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, DefaultFullHttpResponse res) {
+        if (res.getStatus().code() != 200) {
+            ByteBuf buf = Unpooled.copiedBuffer(res.getStatus().toString(), CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+        }
+
+        ChannelFuture f = ctx.channel().writeAndFlush(res);
+        if (res.getStatus().code() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object msg) throws Exception {
+        if (msg instanceof FullHttpRequest) {
+            this.handHttpRequest(channelHandlerContext, (FullHttpRequest) msg);
+        } else if (msg instanceof WebSocketFrame) {
+            this.handWebsocketFrame(channelHandlerContext, (WebSocketFrame) msg);
+        }
+    }
+}
